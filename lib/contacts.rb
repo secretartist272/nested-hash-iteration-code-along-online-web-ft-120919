ad6561081e6e3d7270761c19a3ebce4d6a19e1e4
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-18 06:51:20 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -5,15 +5,24 @@ require 'pry'
   #   "Jon Snow" => {
   #     name: "Jon",
   #     email: "jon_snow@thewall.we", 
-  #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+  #     favorite_ice_cream_flavors: ["chocolate", "strawberry", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
   #     name: "Freddy",
   #     email: "freddy@mercury.com",
-  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  #     favorite_ice_cream_flavors: [ "cookie dough", "mint chip"]
   #   }
   # }
-  
-def remove_strawberry(contacts)
 
+
+def remove_strawberry(contacts)
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
+    end 
+  end
 end
