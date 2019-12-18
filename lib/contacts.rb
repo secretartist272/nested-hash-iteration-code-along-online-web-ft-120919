d3858e63efc70416d432a8b8926d47116b89baa3
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-18 06:49:39 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -5,12 +5,12 @@ require 'pry'
   #   "Jon Snow" => {
   #     name: "Jon",
   #     email: "jon_snow@thewall.we", 
-  #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+  #     favorite_ice_cream_flavors: ["chocolate","strawberry", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
   #     name: "Freddy",
   #     email: "freddy@mercury.com",
-  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  #     favorite_ice_cream_flavors: [ "cookie dough", "mint chip"]
   #   }
   # }
 
